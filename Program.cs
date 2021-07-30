289cf8558f73163eec5ae3a575627c96c1d7af9e
ma77 <velezmathew@gmail.com>
2021-07-29 22:43:23 -0400

    able to list the tasks dir

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
+
+
 namespace main
 {
     class Program
@@ -12,10 +15,24 @@ namespace main
 
             //ask user for their list name (if they have one)
             Console.WriteLine("Do you have a prexisting list?");
-	     
-            
-            //create a file for the user's tasks
+            char usersChoice = Console.ReadLine()[0];
+             
+            if (usersChoice == 'y'){
+                Console.WriteLine("Please select from the following lists");
+                //fetch the current list of task lists in inside of tasks folder and place inside of dir
+                string[] dirs = Directory.GetFiles("tasks");
+                
+                int spot = 0;
+                foreach(string dir in dirs){
+                    Console.WriteLine(spot+". "+dir);
+                    spot++;
+                }
+            }else if(usersChoice == 'n'){
+                Console.WriteLine("What would you like to name your new list?");
 
+            }
+            //create a file for the user's tasks
+             
 
             //ask user for a task to add
             Console.WriteLine("What task would you like to add to your todo list");
